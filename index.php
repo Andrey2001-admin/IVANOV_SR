23b36f45d30fb9bd346e3815b932f7a3a71cc7f4
Andrey <57290383+Andrey2001-admin@users.noreply.github.com>
2022-01-26 18:59:31 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+	require_once "include/session.php"; // на каждой странице
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+	<?php require_once "blocks/head.php"; ?>
+</head>
+
+<body>
+
+	<?php require_once "blocks/header.php"; ?>
+
+	<main >
+	</main>
+
+</body>
+
+</html>
